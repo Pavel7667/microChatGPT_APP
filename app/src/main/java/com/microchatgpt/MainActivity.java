c7da9c0edea0ac962405fe17a19831e524dfb28b
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-05 16:58:11 +0300

    add to MainActivity: callAPI fun -> that do Request to API and get Response back into String addResponse -> Send to Activity text as Bot

--- app/src/main/java/com/microchatgpt/MainActivity.java
+++ app/src/main/java/com/microchatgpt/MainActivity.java
@@ -1,25 +1,45 @@
 package com.microchatgpt;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    TextView textView;
+
+    TextView welcome_text;
     EditText messageEditText;
     ImageButton sendButton;
     List<Message> messageList;
     MessageAdapter messageAdapter;
+
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
 
 
     @Override
@@ -29,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         messageList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_view);
-        textView = findViewById(R.id.welcome_text);
+        welcome_text = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.mesagage_edit_text);
         sendButton = findViewById(R.id.send_btn);
 
@@ -43,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(v -> {
             String question = messageEditText.getText().toString().trim();
             addToChat(question, Message.SENT_BY_ME);
+            messageEditText.setText("");
+            callAPI(question);
+            welcome_text.setVisibility(View.GONE);
         });
     }
 
@@ -56,6 +79,63 @@ public class MainActivity extends AppCompatActivity {
                 messageList.add(new Message(message, sentBy));
                 messageAdapter.notifyDataSetChanged();
                 recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+    }
+
+    /**
+     * Send to Activity text as Bot
+     * @param response from API
+     */
+    void addResponse(String response) {
+        addToChat(response, Message.SENT_BY_BOT);
+    }
+
+    /**
+     * Do Request to API and get Response back into String
+     * @param questions text that sending from chat
+     */
+    void callAPI(String questions) {
+        // okhttp create jsonBody
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model", "text-davinci-003");
+            jsonBody.put("prompt", questions);
+            jsonBody.put("max_tokens", 4000);
+            jsonBody.put("temperature", 0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // create Request using jsonBody / URL / API_KEY
+        RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("POST https://api.openai.com/v1/completions")
+                .header("Authorization", "Bearer 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")
+                .post(body)
+                .build();
+
+        // do Request
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to do Request, case->" + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().toString());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                         e.printStackTrace();
+                    }
+                } else {
+                    addResponse("Failed to give Response, case->" + response.body().toString());
+                }
             }
         });
     }
