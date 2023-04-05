7ebbe8cfbd0d9b3c125f986b18a05f7898f98b6f
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-05 15:44:03 +0300

    add to MainActivity setup recycler view

--- app/src/main/java/com/microchatgpt/MainActivity.java
+++ app/src/main/java/com/microchatgpt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.microchatgpt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,12 +10,17 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     TextView textView;
     EditText messageEditText;
     ImageButton sendButton;
+    List<Message> messagesList;
+    MessageAdapter messageAdapter;
 
 
     @Override
@@ -22,11 +28,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        messagesList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_view);
         textView = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.mesagage_edit_text);
         sendButton = findViewById(R.id.send_btn);
 
+        // setup recycler view
+        messageAdapter = new MessageAdapter(messagesList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
         sendButton.setOnClickListener(v->{
             String question = messageEditText.getText().toString().trim();
