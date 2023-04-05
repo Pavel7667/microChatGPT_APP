dc045f9e9e5598fc2fffab3dc32ce906cb366208
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-05 15:21:26 +0300

    Message.java class as a Model class

---
+++ app/src/main/java/com/microchatgpt/Message.java
@@ -0,0 +1,52 @@
+package com.microchatgpt;
+
+/**
+ * Message class as a Model class
+ */
+public class Message{
+
+    public static String SENT_BY_ME = "me";
+    public static String SENT_BY_BOT = "bot";
+
+    String message;
+    String sentBy;
+
+    // get-set-constructor
+
+    public Message(String message, String sentBy) {
+        this.message = message;
+        this.sentBy = sentBy;
+    }
+
+    public static String getSentByMe() {
+        return SENT_BY_ME;
+    }
+
+    public static void setSentByMe(String sentByMe) {
+        SENT_BY_ME = sentByMe;
+    }
+
+    public static String getSentByBot() {
+        return SENT_BY_BOT;
+    }
+
+    public static void setSentByBot(String sentByBot) {
+        SENT_BY_BOT = sentByBot;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+}
