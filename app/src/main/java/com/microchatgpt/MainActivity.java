37a6a2367f2376cd5455e86b848fec2a7d5d6914
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-05 15:59:08 +0300

    add to MainActivity setup for adding new elements to RV

--- app/src/main/java/com/microchatgpt/MainActivity.java
+++ app/src/main/java/com/microchatgpt/MainActivity.java
@@ -1,14 +1,13 @@
 package com.microchatgpt;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText messageEditText;
     ImageButton sendButton;
-    List<Message> messagesList;
+    List<Message> messageList;
     MessageAdapter messageAdapter;
 
 
@@ -28,22 +27,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        messagesList = new ArrayList<>();
+        messageList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_view);
         textView = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.mesagage_edit_text);
         sendButton = findViewById(R.id.send_btn);
 
         // setup recycler view
-        messageAdapter = new MessageAdapter(messagesList);
+        messageAdapter = new MessageAdapter(messageList);
         recyclerView.setAdapter(messageAdapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        sendButton.setOnClickListener(v->{
+        sendButton.setOnClickListener(v -> {
             String question = messageEditText.getText().toString().trim();
-            Toast.makeText(this,question,Toast.LENGTH_LONG).show();
+            addToChat(question, Message.SENT_BY_ME);
+        });
+    }
+
+    /**
+     * addToChat setup for adding new elements to RV
+     */
+    void addToChat(String message, String sentBy) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
         });
     }
 }
