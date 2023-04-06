765502a09ce5a0110f209b2468f6fed42ac20cc3
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-06 13:43:09 +0300

    android:textIsSelectable="true"

--- app/src/main/java/com/microchatgpt/MainActivity.java
+++ app/src/main/java/com/microchatgpt/MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization", "Bearer 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")
+                .header("Authorization", "Bearer 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")
                 .post(body)
                 .build();
 
